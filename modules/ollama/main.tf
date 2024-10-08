8e71c3fdd2495a87e7d2b08870c7d3c33557213c
George Nicoll <george.nicoll@gmail.com>
2024-10-08 22:50:43 +0100

    Not Working

--- modules/ollama/main.tf
+++ modules/ollama/main.tf
@@ -8,6 +8,7 @@ terraform {
 
 data "docker_registry_image" "ollama" {
   name = "ollama/ollama:latest"
+  #name = "ollama/ollama:rocm"
 }
 
 resource "docker_image" "ollama" {
@@ -15,9 +16,17 @@ resource "docker_image" "ollama" {
   pull_triggers = [data.docker_registry_image.ollama.name]
 }
 
+#docker run -d --restart always --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
+#docker run -d --restart always --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama -e HSA_OVERRIDE_GFX_VERSION=10.3.0 -e HCC_AMDGPU_TARGET=gfx1030 ollama/ollama:rocm
 resource "docker_container" "ollama" {
   name  = "ollama"
   image = docker_image.ollama.image_id
+  devices {
+    host_path = "/dev/kfd"
+  }
+  devices {
+    host_path = "/dev/dri"
+  }
   volumes {
     host_path      = "/var/ollama"
     container_path = "/root/.ollama"
